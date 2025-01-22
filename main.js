a18dbcd11e7d329f586b7e6c7ea4df8d61c2ab29
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-22 13:00:26 -0600

    Began coding to hide applicable fields

---
+++ main.js
@@ -0,0 +1,9 @@
+let country = document.getElementById("country");
+let maritalStatus = document.getElementById("marital-status")
+
+
+function countryVisibility () {
+    if (country.value == "Spain") {
+        
+    }
+}
