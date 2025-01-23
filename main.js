37e1725f52a2c2aa7b23e99f25f480d485df12df
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-23 06:41:31 -0600

    Begin js coding
    Began the coding in js to hide or unhide fields as needed

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 let country = document.getElementById("country");
-let maritalStatus = document.getElementById("marital-status")
+let maritalStatusContainer = document.getElementById("marital-status-container")
+let maritialStatus = document.getElementsByClassName("spain-field")
 
+document.getElementById("country").addEventListener("update", countryVisibility())
 
 function countryVisibility () {
+    console.log("entered countryVisibility function");
+    console.log(country.value);
     if (country.value == "Spain") {
-        
-    }
+        maritalStatusContainer.classList.add(hidden);
+    };
+    return;
 }
